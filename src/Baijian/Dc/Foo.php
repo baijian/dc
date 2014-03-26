61e9233f1896fc6cacf744e8db84a13a5b83f776
柏健 <jian.baij@gmail.com>
2014-03-26 18:48:21 +0800

    add php post log lib

--- src/Baijian/Dc/Foo.php
+++ src/Baijian/Dc/Foo.php
@@ -1,4 +1,33 @@
 <?php
 namespace Baijian\Dc;
+
+use Fluent\Autoloader,
+    Fluent\Logger\FluentLogger,
+    Fluent\Logger\ConsoleLogger,
+    Fluent\Logger\HttpLogger;
+
 class Foo {
+
+    public function __init(){
+        Fluent\Autoloader::register();
+    }
+
+    public function posttcplog($tag, $log){
+        $tag = 'foo.bar';
+        $log = array("hello"=>"world");
+        $logger = new FluentLogger("localhost", "8888");
+        $logger->post($tag, $log);
+    }
+
+    public function posthttplog($tag, $log){
+        $logger = HttpLogger::open("localhost", "8889");
+        $logger->post($tag, $log);
+    }
+
+    public function postconsolelog($tag, $log){
+        $logger = new ConsoleLogger();
+        $logger->post($tag, $log);
+    }
+
+
 }
